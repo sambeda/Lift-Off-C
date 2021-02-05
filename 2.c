ce9d1e42da665cb9c86a81ecf797ebe1184eeec8
sambeda <78551952+sambeda@users.noreply.github.com>
2021-02-06 02:27:24 +0530

    Delete 2.c

--- 2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main() 
-{
-    float C,F;
-    printf("Enter temperature in Celsius: \n");
-    scanf("%f", &C);
-    F=(C*9/5)+32;
-    printf("Temperature in fahrenheit is %f", F);
-}
